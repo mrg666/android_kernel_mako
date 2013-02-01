d98da8a9ccf3ac5e344096ba44c51c997ffca4f3
Hariprasad Dhalinarasimha <hnamgund@codeaurora.org>
2013-02-01 02:32:32 +0000

    qseecom: Release the memory after processing INCOMPLETE_CMD
    Release the memory after processing the INCOMPLETE_CMD,
    as the same memory location is used for processing
    INCOMPLETE_CMD
    
    Change-Id: I9fd305b0e2f272f61b9975d02e222ff543842d7d
    Signed-off-by: Hariprasad Dhalinarasimha <hnamgund@codeaurora.org>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1251,7 +1251,6 @@ static int qseecom_load_commonlib_image(struct qseecom_dev_handle *data)
 	ret = scm_call(SCM_SVC_TZSCHEDULER, 1, &load_req,
 				sizeof(struct qseecom_load_lib_image_ireq),
 							&resp, sizeof(resp));
-	kzfree(img_data);
 	if (ret) {
 		pr_err("scm_call to load failed : ret %d\n", ret);
 		ret = -EIO;
@@ -1277,6 +1276,7 @@ static int qseecom_load_commonlib_image(struct qseecom_dev_handle *data)
 			break;
 		}
 	}
+	kzfree(img_data);
 	return ret;
 }
 
