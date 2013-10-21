ef62543b2e60853b54d923fc52e6717de5e6574d
mrg666 <drgungor@hotmail.com>
2013-10-21 09:12:26 -0400

    Revert "msm: cpufreq: boost freq of online cpu(s) to max on resume"
    This reverts commit 1cb031e902803a9988886a8fba98eaa615e9a049.

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -301,10 +301,6 @@ static int msm_cpufreq_resume(struct cpufreq_policy *policy)
 
 	for_each_possible_cpu(cpu) {
 		per_cpu(cpufreq_suspend, cpu).device_suspended = 0;
-		cpufreq_get_policy(policy, cpu);
-		if (cpu_online(cpu))
-			acpuclk_set_rate(cpu, policy->max, SETRATE_CPUFREQ);
-
 	}
 
 	return 0;
