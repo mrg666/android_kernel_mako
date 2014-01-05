8558151050c817fe7c870f2a179c806ff79ae9e1
mrg666 <drgungor@hotmail.com>
2014-01-05 08:36:29 -0500

    autosmp: use cpu frequency instead of load average - less overhead for the system without the additional load calculation   by autosmp, - autosmp needs to work with cpu governor in giving adequate computing   power to system and CPU governor should have the leading role in this   task since it must respond earlier, - since frequency is a function of CPU load determined by the CPU   governor, autosmp becomes more consistent with the CPU governor when   CPU frequency is used instead of independently calculating the load   average, - works in better sync with the CPU governor by giving the priority   to increasing the frequency before rushing into hotplugging a core or   dragging the system - since autosmp follows the CPU governor it automatically adapts when   governor is changed.

--- arch/arm/kernel/autosmp.c
+++ arch/arm/kernel/autosmp.c
@@ -29,13 +29,9 @@
 #include <linux/cpu.h>
 #include <linux/cpumask.h>
 #include <linux/hrtimer.h>
-#include <linux/rq_stats.h>
 
 #define DEBUG 0
 #define STATS 0
-
-#define DEFAULT_RQ_POLL_JIFFIES		1
-#define DEFAULT_DEF_TIMER_JIFFIES	5
 
 #define ASMP_TAG	"[ASMP]: "
 #define ASMP_STARTDELAY	20000
@@ -53,8 +49,7 @@ static struct asmp_param_struct {
 	bool scroff_single_core;
 	unsigned int max_cpus;
 	unsigned int min_cpus;
-	unsigned int load_limit_up;
-	unsigned int load_limit_down;
+	unsigned int cpufreq_limit;
 	unsigned int cycle_up;
 	unsigned int cycle_down;
 } asmp_param = {
@@ -62,8 +57,7 @@ static struct asmp_param_struct {
 	.scroff_single_core = true,
 	.max_cpus = CONFIG_NR_CPUS,
 	.min_cpus = 1,
-	.load_limit_up = 25,
-	.load_limit_down = 5,
+	.cpufreq_limit = 1000, //MHz
 	.cycle_up = 1,
 	.cycle_down = 5,
 };
@@ -71,76 +65,49 @@ static struct asmp_param_struct {
 static unsigned int cycle;
 static int enabled = 1;
 
-unsigned int get_rq_avg(void) {
-	unsigned long flags = 0;
-	unsigned int rq = 0;
-
-	spin_lock_irqsave(&rq_lock, flags);
-	rq = rq_info.rq_avg;
-	rq_info.rq_avg = 0;
-	spin_unlock_irqrestore(&rq_lock, flags);
-	return rq;
-}
-
-#if CONFIG_NR_CPUS > 2
-static int get_slowest_cpu(void) {
-	int cpu, slow_cpu = 1;
-	unsigned long rate, slow_rate = ULONG_MAX;
-
-	get_online_cpus(); 
-	for_each_online_cpu(cpu)
-		if (cpu > 0) {
-			rate = cpufreq_get(cpu);
-			if (rate < slow_rate) {
-				slow_cpu = cpu;
-				slow_rate = rate;
-			}
-		}
-	put_online_cpus(); 
-	return slow_cpu;
-}
-#endif
-
-static void rq_work_fn(struct work_struct *work) {
-	int64_t diff, now;
-
-	now = ktime_to_ns(ktime_get());
-	diff = now - rq_info.def_start_time;
-	do_div(diff, 1000*1000);
-	rq_info.def_interval = (unsigned int) diff;
-	rq_info.def_timer_jiffies = msecs_to_jiffies(rq_info.def_interval);
-	rq_info.def_start_time = now;
-}
-
 static void __cpuinit asmp_work_fn(struct work_struct *work) {
-	unsigned int cpu = 1;
+	unsigned int cpu, slow_cpu = 0;
+	unsigned int rate, slow_rate = UINT_MAX, fast_rate = 0;
 	int nr_cpu_online;
-	unsigned int rq_avg;
 
 	cycle++;
-	
-	rq_avg = get_rq_avg();
+	/* work in sync with cpu governor to estimate load based on freq */
+	get_online_cpus(); 
 	nr_cpu_online = num_online_cpus();
+	for_each_online_cpu(cpu) {
+		rate = cpufreq_get(cpu);
+		if (cpu) { // count only nonboot cores for slow_rate
+			if (rate <= slow_rate) {
+				slow_cpu = cpu;
+				slow_rate = rate;
+			} else if (rate > fast_rate)
+				fast_rate = rate;
+		} else { // count cpu0 for fast_rate
+			fast_rate = rate;
+		}
+	}
+	put_online_cpus();
+	fast_rate /= 1000;  // kHz -> MHz
+	slow_rate /= 1000;
 
+	/* hotplug one core if one freq is over the limit and no idle cores */
 	if ((nr_cpu_online < asmp_param.max_cpus) && 
-	    (rq_avg >= asmp_param.load_limit_up)) {
+	    (fast_rate > asmp_param.cpufreq_limit) && 
+	    (slow_rate > asmp_param.cpufreq_limit)) {
 		if (cycle >= asmp_param.cycle_up) {
-#if CONFIG_NR_CPUS > 2
 			cpu = cpumask_next_zero(0, cpu_online_mask);
-#endif
 			cpu_up(cpu);
 			cycle = 0;
 #if DEBUG
 			pr_info(ASMP_TAG"CPU[%d] on\n", cpu);
 #endif
 		}
+	/* unplug one core if cpufreq is under the limit */
 	} else if ((nr_cpu_online > asmp_param.min_cpus) &&
-		   (rq_avg <= asmp_param.load_limit_down)) {
-		if (cycle >= asmp_param.cycle_down) {
-#if CONFIG_NR_CPUS > 2
-			cpu = get_slowest_cpu();
-#endif
-			cpu_down(cpu);
+		   (slow_rate < asmp_param.cpufreq_limit)) {
+		if (cycle >= asmp_param.cycle_down) { // but not so soon 
+			if (slow_cpu) 
+				cpu_down(slow_cpu);
 			cycle = 0;
 #if STATS
 			per_cpu(asmp_cpudata, cpu).times_hotplugged += 1;
@@ -158,11 +125,9 @@ static void __cpuinit asmp_work_fn(struct work_struct *work) {
 static void asmp_early_suspend(struct early_suspend *h) {
 	int cpu = 1;
 
-	/* unplug cpu cores */
+	/* unplug online cpu cores */
 	if (asmp_param.scroff_single_core)
-#if CONFIG_NR_CPUS > 2
 		for (cpu = 1; cpu < nr_cpu_ids; cpu++)
-#endif
 			if (cpu_online(cpu)) 
 				cpu_down(cpu);
 
@@ -176,19 +141,17 @@ static void asmp_early_suspend(struct early_suspend *h) {
 static void __cpuinit asmp_late_resume(struct early_suspend *h) {
 	int cpu = 1;
 
-	/* hotplug cpu cores */
+	/* hotplug offline cpu cores */
 	if (asmp_param.scroff_single_core)
-#if CONFIG_NR_CPUS > 2
 		for (cpu = 1; cpu < nr_cpu_ids; cpu++)
-#endif
 			if (!cpu_online(cpu)) 
 				cpu_up(cpu);
 
 	/* resume main work thread */
-	if (enabled) {
+	if (enabled)
 		queue_delayed_work(asmp_workq, &asmp_work, 
 				msecs_to_jiffies(asmp_param.delay));
-	}
+
 	pr_info(ASMP_TAG"autosmp resumed.\n");
 }
 
@@ -209,9 +172,7 @@ static int __cpuinit set_enabled(const char *val, const struct kernel_param *kp)
 		pr_info(ASMP_TAG"autosmp enabled\n");
 	} else {
 		cancel_delayed_work_sync(&asmp_work);
-#if CONFIG_NR_CPUS > 2
 		for (cpu = 1; cpu < nr_cpu_ids; cpu++)
-#endif
 			if (!cpu_online(cpu)) 
 				cpu_up(cpu);
 		pr_info(ASMP_TAG"autosmp disabled\n");
@@ -248,8 +209,7 @@ show_one(delay, delay);
 show_one(scroff_single_core, scroff_single_core);
 show_one(min_cpus, min_cpus);
 show_one(max_cpus, max_cpus);
-show_one(load_limit_up, load_limit_up);
-show_one(load_limit_down, load_limit_down);
+show_one(cpufreq_limit,cpufreq_limit);
 show_one(cycle_up, cycle_up);
 show_one(cycle_down, cycle_down);
 
@@ -268,10 +228,9 @@ static ssize_t store_##file_name					\
 define_one_global_rw(file_name);
 store_one(delay, delay);
 store_one(scroff_single_core, scroff_single_core);
-store_one(max_cpus, max_cpus);
 store_one(min_cpus, min_cpus);
-store_one(load_limit_up, load_limit_up);
-store_one(load_limit_down, load_limit_down);
+store_one(max_cpus, max_cpus);
+store_one(cpufreq_limit, cpufreq_limit);
 store_one(cycle_up, cycle_up);
 store_one(cycle_down, cycle_down);
 
@@ -280,8 +239,7 @@ static struct attribute *asmp_attributes[] = {
 	&scroff_single_core.attr,
 	&min_cpus.attr,
 	&max_cpus.attr,
-	&load_limit_up.attr,
-	&load_limit_down.attr,
+	&cpufreq_limit.attr,
 	&cycle_up.attr,
 	&cycle_down.attr,
 	NULL
@@ -320,18 +278,6 @@ static struct attribute_group asmp_stats_attr_group = {
 static int __init asmp_init(void) {
 	int cpu, rc, err = 0;
 
-	rq_wq = create_singlethread_workqueue("rq_stats");
-	BUG_ON(!rq_wq);
-	INIT_WORK(&rq_info.def_timer_work, rq_work_fn);
-	spin_lock_init(&rq_lock);
-	rq_info.rq_poll_jiffies = DEFAULT_RQ_POLL_JIFFIES;
-	rq_info.def_timer_jiffies = DEFAULT_DEF_TIMER_JIFFIES;
-	rq_info.def_start_time = ktime_to_ns(ktime_get());
-	rq_info.rq_poll_last_jiffy = 0;
-	rq_info.def_timer_last_jiffy = 0;
-	rq_info.hotplug_disabled = 0;
-	rq_info.init = 1;
-
 	cycle = 0;
 	for_each_possible_cpu(cpu)
 		per_cpu(asmp_cpudata, cpu).times_hotplugged = 0;
@@ -350,14 +296,14 @@ static int __init asmp_init(void) {
 	if (asmp_kobject) {
 		rc = sysfs_create_group(asmp_kobject, &asmp_attr_group);
 		if (rc)
-			pr_warn(ASMP_TAG"sysfs: ERROR, could not create sysfs group");
+			pr_warn(ASMP_TAG"ERROR, create sysfs group");
 #if STATS
 		rc = sysfs_create_group(asmp_kobject, &asmp_stats_attr_group);
 		if (rc)
-			pr_warn(ASMP_TAG"sysfs: ERROR, could not create sysfs stats group");
+			pr_warn(ASMP_TAG"ERROR, create sysfs stats group");
 #endif
 	} else
-		pr_warn(ASMP_TAG"sysfs: ERROR, could not create sysfs kobj");
+		pr_warn(ASMP_TAG"ERROR, create sysfs kobj");
 
 	pr_info(ASMP_TAG"%s init complete.", __func__);
 	return err;
