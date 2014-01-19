e3498b48baa646953d1d7e8db0e26c2c6b26f112
mrg666 <drgungor@hotmail.com>
2014-01-19 11:58:35 -0500

    alpha: take a bunch of syscalls into osf_sys.c
    New helper: current_thread_info().  Allows to do a bunch of odd syscalls
    in C. While we are at it, there had never been a reason to do
    osf_getpriority() in assembler.  We also get "namespace"-aware (read:
    consistent with getuid(2), etc.) behaviour from getx?id() syscalls now.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Michael Cree <mcree@orcon.net.nz>
    Acked-by: Matt Turner <mattst88@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/timer.c
+++ kernel/timer.c
@@ -1404,13 +1404,6 @@ SYSCALL_DEFINE1(alarm, unsigned int, seconds)
 
 #endif
 
-#ifndef __alpha__
-
-/*
- * The Alpha uses getxpid, getxuid, and getxgid instead.  Maybe this
- * should be moved into arch/i386 instead?
- */
-
 /**
  * sys_getpid - return the thread group id of the current process
  *
@@ -1465,8 +1458,6 @@ SYSCALL_DEFINE0(getegid)
 	/* Only we change this so SMP safe */
 	return  current_egid();
 }
-
-#endif
 
 static void process_timeout(unsigned long __data)
 {
