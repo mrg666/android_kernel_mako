c47649046410e92ba0bd3ba7929c02963bb34c0c
mrg666 <drgungor@hotmail.com>
2014-01-21 14:42:06 -0500

    msm: pm-8x60: Increase warn timeout *further* for sleep sts check

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -932,7 +932,7 @@ int msm_pm_wait_cpu_shutdown(unsigned int cpu)
 		if (acc_sts & msm_pm_slp_sts[cpu].mask)
 			return 0;
 		udelay(100);
-		WARN(++timeout == 20, "CPU%u didn't collape within 2ms\n",
+		WARN(++timeout == 50, "CPU%u didn't collape within 5ms\n",
 					cpu);
 	}
 
