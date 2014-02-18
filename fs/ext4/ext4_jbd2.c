ba29ca265bd1fb7521333ac2bb465fb94b0c770c
mrg666 <drgungor@hotmail.com>
2014-02-18 09:39:12 -0500

    ext4: remove unnecessary superblock dirtying
    This patch changes the 'ext4_handle_dirty_super()' function which
    submits the superblock for I/O in the following cases:
    
    1. When creating the first large file on a file system without
       EXT4_FEATURE_RO_COMPAT_LARGE_FILE feature.
    2. When re-sizing the file-system.
    3. When creating an xattr on a file-system without the
       EXT4_FEATURE_COMPAT_EXT_ATTR feature.
    
    If the file-system has journal enabled, the superblock is written via
    the journal. We do not modify this path.
    
    If the file-system has no journal, this function, falls back to just
    marking the superblock as dirty using the 's_dirt' superblock
    flag. This means that it delays the actual superblock I/O submission
    by 5 seconds (default setting).  Namely, the 'sync_supers()' kernel
    thread will call 'ext4_write_super()' later and will actually submit
    the superblock for I/O.
    
    And this is the behavior this patch modifies: we stop using 's_dirt'
    and just mark the superblock buffer as dirty right away. Indeed, all 3
    cases above are extremely rare and it does not add any value to delay
    the I/O submission for them.
    
    Note: 'ext4_handle_dirty_super()' executes
    '__ext4_handle_dirty_super()' with 'now = 0'. This patch basically
    makes the 'now' argument unneeded and it will be deleted in one of the
    next patches.
    
    This patch also removes 's_dirt' condition on the unmount path because
    we never set it anymore, so we should not test it.
    
    Tested using xfstests for both journalled and non-journalled ext4.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Reviewed-by: Jan Kara <jack@suse.cz>

--- fs/ext4/ext4_jbd2.c
+++ fs/ext4/ext4_jbd2.c
@@ -148,7 +148,6 @@ int __ext4_handle_dirty_super(const char *where, unsigned int line,
 		if (err)
 			ext4_journal_abort_handle(where, line, __func__,
 						  bh, handle, err);
-	} else
-		sb->s_dirt = 1;
+	}
 	return err;
 }
